ac6dd1cf14af0c7a2ef0e579a807b687c583d18f
shishkin_sv <shishkin_sv@ursip.ru>
2021-12-16 19:48:40 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Container from "./Container";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Container />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/covid-cert/status/d0asvdd1d8f-78cf9-43f68-93ee2-40cbf436as3a"
+          element={<Container />}
+        />
+        <Route
+          path="/"
+          element={
+            <Navigate to="/covid-cert/status/d0asvdd1d8f-78cf9-43f68-93ee2-40cbf436as3a" />
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
